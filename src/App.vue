d9a4ada694897ff2782e9e37183ec29953f513f6
appleseed619 <sungan_555_3@outlook.com>
2020-05-22 22:51:51 -0400

    store

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <TopNavigation/>
+    <div class="content-wrapper">
+      <router-view/>
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import TopNavigation from '@/components/TopNavigation'
+import Footer from '@/components/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    Footer,
+    TopNavigation
+  }
+}
+</script>
+<style> @import './assets/styles.css'; </style>
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import actions from './actions'
+import getters from './getters'
+import mutations from './mutations'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state : { // = data
+    products:[],
+    cart:[],
+    cartItems:0,
+    checkoutStatus: null,
+    highprice: 1500,
+    sale: false
+  },
+  getters,
+  actions,
+  mutations
+})
